b02840955da3931c296a6e5ff77dc8ab42f799b6
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2025-01-12 08:51:07 +0900

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
     commonjsOptions: {
       include: [/node_modules/],
       transformMixedEsModules: true
-    }
+    },
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'ESLINT_ERROR') return; // ESLint 오류 무시
+        warn(warning);
+      },
+    },
   }
 })
