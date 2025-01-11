c722c1e744116e137645ed90d7430c89cf66ab35
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-01-11 17:07:43 +0900

    initial commit

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+import { queryClient } from '@apis/queryClient'
+
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 )
---
+++ apps/web/src/pages/Add/Add.tsx
@@ -0,0 +1,3 @@
+export function Add() {
+  return <div>Add</div>
+}
