95987412698b616d5ed45f3fe6376f2ab504ebb1
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-01-11 19:13:32 +0900

    feat: navbar, story 추가

--- apps/web/src/components/common/CustomIcons.tsx
+++ apps/web/src/components/common/CustomIcons.tsx
@@ -1,14 +1,19 @@
 import { UserIcon } from '@heroicons/react/16/solid'
-import { ArrowLeftStartOnRectangleIcon } from '@heroicons/react/24/outline'
+import {
+  ArrowLeftStartOnRectangleIcon,
+  ChevronLeftIcon
+} from '@heroicons/react/24/outline'
 
 interface CustomIcons {
   UserIcon: typeof UserIcon
   LogoutIcon: typeof ArrowLeftStartOnRectangleIcon
+  ChevronBackIcon: typeof ChevronLeftIcon
 }
 
 const CustomIcons: CustomIcons = {
   UserIcon,
-  LogoutIcon: ArrowLeftStartOnRectangleIcon
+  LogoutIcon: ArrowLeftStartOnRectangleIcon,
+  ChevronBackIcon: ChevronLeftIcon
 }
 
 export default CustomIcons
